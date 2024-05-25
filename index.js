ec89b1bdd0a853ea08875b52e619a42fe109781e
Salman Malik <ssmalik989@gmail.com>
2024-05-25 16:43:08 +0530

    worked on mobile for fixed height while on click

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const hamburgerMenu = document.querySelector(".fa-solid");
 const navbar = document.querySelector(".header-nav ul");
+const container = document.querySelector(".container");
 
 function toggleNav() {
   navbar.classList.toggle("show");
+  container.classList.toggle("height");
+  hamburgerMenu.classList.toggle("fa-bars");
+  hamburgerMenu.classList.toggle("fa-xmark");
 }
 
 hamburgerMenu.addEventListener("click", toggleNav);
